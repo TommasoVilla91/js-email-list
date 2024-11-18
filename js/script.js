ddad522669a4c7542e49eec66d701e747233be8b
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-18 17:31:17 +0100

    feat: exercise completed

--- js/script.js
+++ js/script.js
@@ -3,13 +3,21 @@ const emailListElem = document.getElementById("email-list");
 
 
 // logica
+let rndEmails = "";
+
 for (let i = 0; i < 10; i++) {
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
         console.log(resp);
 
         const email = resp.data.response;
         console.log(email);
-    
+        
+        rndEmails += email + " ";
+        console.log(rndEmails);
+        
+        const listItem = document.createElement("li");
+        listItem.textContent = email;
+        emailListElem.append(listItem);
     });
 };
 
