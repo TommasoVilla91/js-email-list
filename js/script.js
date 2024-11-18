1f2cddf1e3bbeb916ab602a25c0f29ff0564f8e2
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-18 18:11:50 +0100

    feat: css implemented

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
-// Dichiarazioni
+// DICHIARAZIONI
 const emailListElem = document.getElementById("email-list");
 
 
-// logica
+// LOGICA
+// variabile d'appoggio per raggiuppare tutte le mail
 let rndEmails = "";
 
+// creo 10 mail random
 for (let i = 0; i < 10; i++) {
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
         console.log(resp);
 
+        // tra tutti i dati forniti, seleziono la mail
         const email = resp.data.response;
         console.log(email);
         
+        // le inserisco nella variabile
         rndEmails += email + " ";
         console.log(rndEmails);
         
+        // creo il tag dove inserirle
         const listItem = document.createElement("li");
         listItem.textContent = email;
+
+        // OUTPUT
+        // stampo in pagina
         emailListElem.append(listItem);
     });
 };
